f3f6e162aa21740819849a25ef2bc44486f13633
Jeremy <jeremygan@JeremydeMac-mini.local>
2025-05-31 22:36:07 +0800

    first commit

--- main.py
+++ main.py
@@ -1027,7 +1027,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app", 
         host="0.0.0.0",
-        port=88, 
+        port=8888, 
         reload=True,
         access_log=True
     )
